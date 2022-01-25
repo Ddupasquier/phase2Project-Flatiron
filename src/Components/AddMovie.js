622c50737790dcfb49e6645b8e998b9e28bbb8cd
dylandupasquier <dylandupasquier@gmail.com>
2022-01-25 10:03:57 -0800

    form fixed

--- src/Components/AddMovie.js
+++ src/Components/AddMovie.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-<<<<<<< HEAD
 function AddMovie() {
   const [title, setTitle] = useState("");
   const [rating, setRating] = useState();
@@ -13,23 +12,13 @@ function AddMovie() {
   const [trailer, setTrailer] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
-=======
->>>>>>> f192856d4893af16c139b6fabdffd9d0aee7958e
 
-function AddMovie() {
-    const [title, setTitle] = useState("");
-    const [rating, setRating] = useState("");
-    const [runtime, setRuntime] = useState("");
-    const [release, setRelease] = useState("");
-    const [director, setDirector] = useState("");
-    const [writers, setWriters] = useState("");
-    const [actors, setActors] = useState([]);
-    const [poster, setPoster] = useState("");
-    const [trailer, setTrailer] = useState("");
-    const [movieSubmission, setMovieSubmission] = useState({});
-    function handleSubmit(e) {
-      e.preventDefault();
-      setMovieSubmission({
+    fetch("http://localhost:8001/movies", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         title: title,
         rating: rating,
         runtime: runtime,
@@ -39,90 +28,103 @@ function AddMovie() {
         actors: actors,
         poster: poster,
         trailer: trailer,
+      }),
+    })
+      .then((r) => r.json())
+      .then(() => {
+        setTitle("");
+        setRuntime("");
+        setDirector("");
+        setWriters("");
+        setActors([]);
+        setRating("");
+        setPoster("");
+        setTrailer("");
+        setRelease("");
       });
-    }
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={title}
-          />
-          <br />
-          <input
-            onChange={(e) => setRuntime(e.target.value)}
-            type="text"
-            name="runtime"
-            placeholder="Runtime"
-            value={runtime}
-          />
-          <br />
-          <input
-            onChange={(e) => setDirector(e.target.value)}
-            type="text"
-            name="director"
-            placeholder="Director"
-            value={director}
-          />
-          <br />
-          <input
-            onChange={(e) => setWriters(e.target.value)}
-            type="text"
-            name="writers"
-            placeholder="Writers"
-            value={writers}
-          />
-          <br />
-          <input
-            onChange={(e) => setActors(e.target.value.split(","))}
-            type="text"
-            name="actors"
-            placeholder="Actors"
-            value={actors}
-          />
-          <br />
-          <input
-            onChange={(e) => setPoster(e.target.value)}
-            type="text"
-            name="poster"
-            placeholder="Poster"
-            value={poster}
-          />
-          <br />
-          <input
-            onChange={(e) => setTrailer(e.target.value)}
-            type="text"
-            name="trailer"
-            placeholder="Trailer"
-            value={trailer}
-          />
-          <br />
-          <label htmlFor="select">Select Rating: </label>
-          <select onChange={(e) => setRating(e.target.value)} value={rating}>
-            <option value=""></option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-            <option value="F">F</option>
-          </select>
-          <br />
-          <label htmlFor="release">Set Release Date</label>
-          <br />
-          <input
-            onChange={(e) => setRelease(e.target.value)}
-            type="date"
-            name="release"
-            value={release}
-          />
-          <br />
-          <button type="submit">Submit Movie</button>
-        </form>
-      </div>
-    );
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={title}
+        />
+        <br />
+        <input
+          onChange={(e) => setRuntime(e.target.value)}
+          type="text"
+          name="runtime"
+          placeholder="Runtime"
+          value={runtime}
+        />
+        <br />
+        <input
+          onChange={(e) => setDirector(e.target.value)}
+          type="text"
+          name="director"
+          placeholder="Director"
+          value={director}
+        />
+        <br />
+        <input
+          onChange={(e) => setWriters(e.target.value)}
+          type="text"
+          name="writers"
+          placeholder="Writers"
+          value={writers}
+        />
+        <br />
+        <input
+          onChange={(e) => setActors(e.target.value.split(","))}
+          type="text"
+          name="actors"
+          placeholder="Actors"
+          value={actors}
+        />
+        <br />
+        <input
+          onChange={(e) => setPoster(e.target.value)}
+          type="text"
+          name="poster"
+          placeholder="Poster"
+          value={poster}
+        />
+        <br />
+        <input
+          onChange={(e) => setTrailer(e.target.value)}
+          type="text"
+          name="trailer"
+          placeholder="Trailer"
+          value={trailer}
+        />
+        <br />
+        <label htmlFor="select">Select Rating: </label>
+        <select onChange={(e) => setRating(e.target.value)} value={rating}>
+          <option value=""></option>
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+          <option value="D">D</option>
+          <option value="F">F</option>
+        </select>
+        <br />
+        <label htmlFor="release">Set Release Date</label>
+        <br />
+        <input
+          onChange={(e) => setRelease(e.target.value)}
+          type="date"
+          name="release"
+          value={release}
+        />
+        <br />
+        <button type="submit">Submit Movie</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddMovie;

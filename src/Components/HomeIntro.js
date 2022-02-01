2a40318f97ec4704bd197d11cf0783dabc0d2d22
dylandupasquier <dylandupasquier@gmail.com>
2022-01-31 17:40:35 -0800

    updated

--- src/Components/HomeIntro.js
+++ src/Components/HomeIntro.js
@@ -13,6 +13,7 @@ function HomeIntro() {
           style={{ height: "50rem", width: "70rem" }}
           src="https://observer.com/wp-content/uploads/sites/2/2021/05/summer-movies-2021-new-e1620919489437.jpg?quality=80"
           alt="home logo"
+          className="homeImg"
         ></img>
       </main>
     </div>
